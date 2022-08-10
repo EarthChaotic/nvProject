a8d4afe9ad735e54adc9681856e5464a9a30e0e6
EarthChaotic <jeraponteesup@gmail.com>
2022-08-10 12:08:13 +0700

    8/10/2022

--- Screens/HomeScreen.js
+++ Screens/HomeScreen.js
@@ -1,30 +1,26 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import { Ionicons } from '@expo/vector-icons';
 
-const HomeScreen = () => {
+const HomeScreen = ({navigation}) => {
   return (
-    <View>
-      <Text>HomeScreen</Text>
+    <View style={{flex:1,alignItems:'center',justifyContent:'center'}}>
+      <Ionicons
+      name='home'
+      size={30}
+      color='#00812'
+      />
+      <Text>Home Screen</Text>
+      <Button 
+      title='เกี่ยวกับเรา'
+      onPress={()=>navigation.navigate('About',
+      {email:'tr.jirapon_st@tni.ac.th'}
+      )}
+      />
     </View>
   )
 }
 
 export default HomeScreen
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: 'center',
-        padding: 20,
-    },
-    heading: {
-        fontSize: 25,
-        textAlign: 'center',
-        marginVertical: 10,
-    },
-    textStyle: {
-        textAlign: 'center',
-        fontSize: 16,
-        marginVertical: 10,
-    },
-})
