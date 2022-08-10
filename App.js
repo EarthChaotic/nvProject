2aa382cd9ca8cbb86c99cee902b18f4f75e1475e
EarthChaotic <jeraponteesup@gmail.com>
2022-08-10 11:40:44 +0700

    8/10/2022

--- App.js
+++ App.js
@@ -1,52 +1,53 @@
-import { StatusBar } from 'expo-status-bar';
-import { Text, View, Button, TextInput } from 'react-native';
+import { StyleSheet, Text, View,Button,TextInput } from 'react-native'
+import React from 'react'
 
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
 
-function HomeScreen({ navigation, route }) {
+function HomeScreen({navigation,route}){
 
-  React.useEffect(() => {
-    if (route.params?.post) {
-      //Post updated,do somethin with 'route.params.post'
-      //For example, sent the post to the server      
+  React.useEffect(()=>{
+
+    if(route.params?.post){
+      //Post update, do something with 'route,params.post'
+      //for example, sent post to the server
+
     }
-  }, [route.params?.post]);
+  },[route.params?.post]);
 
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+  return(
+    <View style={{flex:1,alignItems:'center',justifyContent:'center'}}>
       <Button
         title='Create Post'
-        onPress={() => navigation.navigate('CreatePost')}
+        onPress={()=>navigation.navigate('CreatePost')}
       />
-      <Text style={{ margin: 10 }}>Post:{route.params?.post}</Text>
+      <Text style={{margin:10}}>Post:{route.params?.post}</Text>
     </View>
   );
 }
 
-function CreatePostScreen({ navigation, route }) {
-
-  const [postText, setPostText] = React.useState('');
-
-  return (
-    // use Fragment
+function CreatePostScreen({navigation,route}){ 
+  
+  const [postText,setPostText] = React.useState('');
+ 
+  return(
     <>
-      <TextInput
-        multiline
-        placeholder='Plese text here'
-        style={{ height: 200, padding: 10, backgroundColor: 'white' }}
-        onChangeText={setPostText}
-        value={postText}
-      />
-      <Button
-        title='Click'
-        onPress={() => {
-          //Pass params back to HomeScreen funchion
-          navigation.navigate('Home', { post: postText })
-        }}
-      />
+    <TextInput
+    multiline
+    placeholder='Text Here'
+    style={{height:200,padding:10,backgroundColor:"white"}}
+    onChangeText={setPostText}
+    value = {postText}
+    />
+    <Button
+    title='Click'
+    onPress={()=>{
+      //Pass Params to Home Screen
+      navigation.navigate('Home',{post:postText})
+    }}
+    
+    />
     </>
 
   );
@@ -58,15 +59,15 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator
-        initialRouteName='Home'
-        screenOptions={{
-          headerStyle: { backgroundColor: 'blue' },
-          headerTintColor: '#ffff',
-          headerTitleStyle: { fontWeight: 'bold' }
-        }}
+       initialRouteName='Home'
+      screenOptions={{
+        headerStyle: { backgroundColor:'#FE0000'},
+        headerTintColor:'#fff',
+        headerTitleStyle:{fontWeight:'bold',fontSize:25}
+      }}
       >
-        <Stack.Screen name='Home' component={HomeScreen} />
-        <Stack.Screen name='CreatePost' component={CreatePostScreen} />
+        <Stack.Screen name ='Home' component = {HomeScreen}/>
+        <Stack.Screen name ='CreatePost' component = {CreatePostScreen}/>
       </Stack.Navigator>
     </NavigationContainer>
 
