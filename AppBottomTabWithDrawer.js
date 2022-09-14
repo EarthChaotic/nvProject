75cf5940e7d4d6d998245acf4e8c19b08066769b
EarthChaotic <jeraponteesup@gmail.com>
2022-09-14 11:25:34 +0700

    9/14/2022

---
+++ AppBottomTabWithDrawer.js
@@ -0,0 +1,95 @@
+import { View, Text, Button,SafeAreaView , Image} from "react-native";
+import React from "react";
+import { NavigationContainer,DefaultTheme, StackActions} from "@react-navigation/native";
+import { Ionicons } from "@expo/vector-icons";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import {
+  createDrawerNavigator,
+  DrawerContentScrollView,
+  DrawerItemList,
+  DrawerItem,
+} from "@react-navigation/drawer";
+import HomeScreen from "./Screens/HomeScreen";
+const MyTheme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: "rgb(255,45,85)",
+  },
+};
+
+function CustomDrawerContent(props) {
+  return (
+    <SafeAreaView>
+      <Image
+        source={require("./assets/react_logo.png")}
+      />
+      <DrawerContentScrollView {...props}>
+        <DrawerItemList {...props} />
+      </DrawerContentScrollView>
+    </SafeAreaView>
+  );
+}
+const Drawer = createDrawerNavigator();
+
+function MyDrawer() {
+  return (
+    <Drawer.Navigator
+      useLegacyImplementation
+      drawerContent={(props) => <CustomDrawerContent {...props} />}
+      screenOptions={{
+        drawerStyle: {
+          width: 240,
+        },
+      }}
+    >
+      <Drawer.Screen name="Home" component={MyTab}/>
+      {/* <Drawer.Screen name="Feed" component={FeedScreen} /> */}
+      <Drawer.Screen name="Setting" component={MyTab} />
+    </Drawer.Navigator>
+  );
+}
+
+function SettingScreen({navigation}) {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Setting!</Text>
+      <Button title="Go to Home" onPress={()=>navigation.navigate("Home")}/>
+    </View>
+  );
+}
+const Tab = createBottomTabNavigator();
+
+function MyTab() {
+  return (
+    <Tab.Navigator  screenOptions={({ route }) => ({
+      tabBarIcon: ({focused, color, size}) => {
+        let iconName;
+        if(route.name ==='Home') {
+          iconName = focused
+          ? 'ios-information-circle'
+          : 'ios-information-circle-outline'
+        }else if (route.name === 'Setting'){
+          iconName = focused ? 'ios-list-box' : 'ios-list'
+        }
+        return <Ionicons name={iconName} size={size} color={color}/>
+      },
+      tabBarActiveTintColor:'tomato',
+      tabBarInactiveTintColor:'gray'
+    })}>
+      <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen name="Setting" component={SettingScreen} />
+    </Tab.Navigator>
+  );
+}
+
+const App = () => {
+  return (
+    <NavigationContainer theme={MyTheme}>
+      <MyDrawer/>
+    </NavigationContainer>
+  );
+};
+
+export default App;
